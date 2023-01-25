0ff059e3c22425741269ed0b9046797b538f31d1
satyamkushwaha <satyamkushwaha1101@gmail.com>
2023-01-25 14:54:25 +0530

    done build

--- blogpostapp/src/components/Header/index.jsx
+++ blogpostapp/src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header_container '>
-            <h1 className='ms-5'>Eqaim Blog</h1>
+            <h1 className='ms-5'>Blog Creator and Editor</h1>
         </div>
     )
 }
